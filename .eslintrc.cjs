b152723f751d95ed1fcee47a1d6e82c880c8b2c8
91nickel <91nickel@gmail.com>
2024-03-25 18:54:40 +0300

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            {allowConstantExport: true},
+        ],
+        'indent': ['error', 4],
+        'curly': ['off'],
+        'space-before-function-paren': 0,
+        'object-curly-spacing': 0,
+        'react/no-unescaped-entities': 0,
+        'no-extra-boolean-cast': 'off',
+        'comma-dangle': 'off',
+        // 'no-unused-vars': 0,
+    },
+}
