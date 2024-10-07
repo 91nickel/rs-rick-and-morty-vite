3e69d7c3f55342a7f9b1e681ccb45af0aeee697e
91nickel <91nickel@gmail.com>
2024-10-07 15:32:16 +0300

    pwa prod build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { normalizePath } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,18 +13,26 @@ export default defineConfig({
         },
     },
     plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: normalizePath(path.resolve(__dirname, './src/assets/')),
+                    dest: normalizePath(path.resolve(__dirname, './dist/src'))
+                }
+            ]
+        }),
         react(),
         VitePWA({
             registerType: 'autoUpdate',
             devOptions: {
-                enabled: true
+                enabled: true,
             },
-            base: "/",
-            srcDir: "src",
-            filename: "sw.ts",
+            base: '/',
+            srcDir: 'src',
+            filename: 'sw.ts',
             injectRegister: 'auto',
             includeAssets: ['favicon.ico'],
-            strategies: "injectManifest",
+            strategies: 'injectManifest',
             injectManifest: {
                 minify: false,
                 enableWorkboxModulesLogs: true,
@@ -32,42 +42,42 @@ export default defineConfig({
                 short_name: 'Rick&Morty',
                 description: 'Result School example app',
                 theme_color: '#FFFFFF',
-                display: "standalone",
-                start_url: "/",
+                display: 'standalone',
+                start_url: '/',
                 orientation: 'portrait-primary',
                 icons: [
                     {
-                        "src": "/src/assets/icons/512-512.png",
-                        "sizes": "512x512",
-                        "type": "image/png",
-                        "purpose": "any maskable"
+                        'src': '/src/assets/icons/512-512.png',
+                        'sizes': '512x512',
+                        'type': 'image/png',
+                        'purpose': 'any maskable',
                     },
                     {
-                        "src": "/src/assets/icons/192-192.png",
-                        "sizes": "192x192",
-                        "type": "image/png",
+                        'src': '/src/assets/icons/192-192.png',
+                        'sizes': '192x192',
+                        'type': 'image/png',
                     },
                     {
-                        "src": "/src/assets/icons/144-144.png",
-                        "sizes": "144x144",
-                        "type": "image/png",
+                        'src': '/src/assets/icons/144-144.png',
+                        'sizes': '144x144',
+                        'type': 'image/png',
                     },
                     {
-                        "src": "/src/assets/icons/96-96.png",
-                        "sizes": "96x96",
-                        "type": "image/png",
+                        'src': '/src/assets/icons/96-96.png',
+                        'sizes': '96x96',
+                        'type': 'image/png',
                     },
                     {
-                        "src": "/src/assets/icons/72-72.png",
-                        "sizes": "72x72",
-                        "type": "image/png",
+                        'src': '/src/assets/icons/72-72.png',
+                        'sizes': '72x72',
+                        'type': 'image/png',
                     },
                     {
-                        "src": "/src/assets/icons/48-48.png",
-                        "sizes": "48x48",
-                        "type": "image/png",
-                    },]
-            }
+                        'src': '/src/assets/icons/48-48.png',
+                        'sizes': '48x48',
+                        'type': 'image/png',
+                    }],
+            },
         }),
     ],
 })
